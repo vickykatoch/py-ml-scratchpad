77f6f3e78bda8b5e10c2b1af43a02617df3505cb
vickykatoch <vickykatoch@yahoo.com>
2020-04-29 22:33:58 -0400

    lin reg

--- regression/linear/01-simple-linear-regression.py
+++ regression/linear/01-simple-linear-regression.py
@@ -12,18 +12,22 @@ def plotData(x, y, h):
 def calcR2(actual_y,predicted_y,avg_y):
     return np.square(predicted_y - avg_y).sum()/np.square(actual_y - avg_y).sum()
 
+def calcSlope(x,y,mean_x,mean_y):
+    numerator = np.sum((x - mean_x) * (y - mean_y))
+    denominator = np.sum(np.square(x - mean_x))
+    return numerator/denominator
+
 X = np.array([1,2,3,4,5])
 Y = np.array([3,4,2,4, 5])
 
 mean_x = np.mean(X)
 mean_y = np.mean(Y)
-numerator = np.sum((X - mean_x) * (Y - mean_y))
-denominator = np.sum(np.square(X - mean_x))
-slope_m = numerator/denominator
+slope_m = calcSlope(X,Y, mean_x, mean_y)
 intercept_c = mean_y - mean_x * slope_m
 m = len(X)
 H=[]
 for i in range(0,m):
     H.append(slope_m * X[i] + intercept_c)
-print("Slope : {}, Y Intercept : {}, Prediction Accuracy (R2 Error) : {}".format(slope_m,intercept_c,calcR2(Y,H,mean_y)))  
+
+print("Slope : {}, Y Intercept : {}, Prediction Accuracy (R2 Error) : {}".format(slope_m,intercept_c,calcR2(Y,H,mean_y))) 
 plotData(X,Y,H)
