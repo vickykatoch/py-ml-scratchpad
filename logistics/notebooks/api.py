0327d4335a241a8deb507375c6b81b19f2472cb2
vickykatoch <vickykatoch@yahoo.com>
2020-05-31 15:40:07 -0400

    classifier

--- logistics/notebooks/api.py
+++ logistics/notebooks/api.py
@@ -18,3 +18,22 @@ def gradient(theta, X,y):
     hx = sigmoid(hypothesis(X,theta.reshape(len(theta),1)))
     g = ((hx - y).T @ X) * 1/m
     return g.flatten() if theta.ndim==1 else g
+
+def map_features(x1,x2,degree):
+    features = np.ones((len(x1),1))
+    for i in range(1,degree+1):        
+        for j in range(0,i+1):
+            z = np.power(x1 , (i-j)) * np.power(x2 , j)
+            features = np.hstack([features,z])
+    return features
+
+def cost_regularized(theta, X, y, lmbda):
+    m,n = X.shape
+    n_cost = cost(theta,X,y)
+    n_cost += np.sum((theta[2:len(theta):] ** 2)) * (lmbda/(2*m))
+    return n_cost
+
+def gradient_regularized(theta,X,y,lmbda):
+    grad = gradient(theta,X,y)
+    m,n = X.shape
+    return grad + np.hstack([[[0]], ((lmbda/m) *  theta[1:,:]).T])
--- logistics/processing/logistic_reg2.py
+++ logistics/processing/logistic_reg2.py
@@ -3,23 +3,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as op
 from os import path
+import logistics.core.api as api
 #endregion
 
 #region funcs
-def visualize(X):
-    true_recs = X[X[:,3] == 1,:]
-    false_recs = X[X[:,3] == 0,:]
+def visualize(X,theta, yIdx,title):
+    true_recs = X[X[:,yIdx] == 1,:]
+    false_recs = X[X[:,yIdx] == 0,:]
+    x,y,z = get_decision_boundary(theta)
+    x,y =np.meshgrid(x,y)
+
     plt.figure(figsize=(16,8))
-    plt.scatter(true_recs[:,1],true_recs[:,2],marker='+',color='g')
-    plt.scatter(false_recs[:,1],false_recs[:,2],marker='o',color='r')
+    cp = plt.contour(x,y,z, levels=[0.0],colors='b', linestyles='dashed', linewidths=1)
+    plt.clabel(cp, inline=1, fontsize=10)
+    plt.scatter(true_recs[:,0],true_recs[:,1],marker='+',color='g')
+    plt.scatter(false_recs[:,0],false_recs[:,1],marker='o',color='r')
     plt.xlabel('Test 1 scores')
     plt.ylabel('Test 2 scores')
-    plt.legend(('Passed', 'Not Passed'), bbox_to_anchor=(1.15, 1))
+    plt.title(title)
+    plt.legend(('Passed', 'Not Passed','DB'))
     plt.grid(color='gray', linestyle='--', linewidth=.6, axis='both', which='both', alpha=.4)
     plt.show()
+
+def get_decision_boundary(theta):
+    u = np.linspace(-1, 1.5, 50).reshape(50,1)
+    v = np.linspace(-1, 1.5, 50).reshape(50,1)
+    z = np.zeros((len(u), len(v)))
+    for i in range(0,len(u)):
+        for j in range(0,len(v)):
+            z[i,j]=api.map_features([u[i,:]], [v[j,:]],6) @ theta
+    return (u,v, z.T)
+
+def process(lmda):
+    data = np.genfromtxt(path.join(path.dirname(__file__),'../data/raw/ex2data2.txt'),delimiter=",")   
+    X = api.map_features(data[:,0].reshape(len(data),1), data[:,1].reshape(len(data),1),6)
+    m,n=X.shape
+    y = data[:,2].reshape(m,1)
+    theta = np.zeros((n,1))
+    res = op.minimize(fun=api.cost_regularized,x0=theta,args=(X,y,lmda),method='CG',options={'maxiter': 400})
+    theta = res.x.reshape(len(res.x),1)
+    visualize(data,theta,2,f'Lamda : {lmda}')
+
 #endregion
 
+
+
 def run():
-    data = data = np.genfromtxt(path.join(path.dirname(__file__),'../data/raw/ex2data2.txt'),delimiter=",")
-    X = np.hstack([np.ones((len(data),1)), data])
-    visualize(X)
+    # Just right
+    process(1)
+    # Underfit
+    process(100)
+    # Overfit
+    process(0)
+
+    
